a75340e8b0e9c3b8709f8fda6d3cc642915a8cc9
Chris <cjcorbin@gmail.com>
2020-08-22 21:39:54 -0700

    Code Tidying

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
 import formSchema from "./validation/formSchema";
 import * as yup from "yup";
